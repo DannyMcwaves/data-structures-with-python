3bf03fe20deb22b543c6bf836ca204d597f29295
dannymcwaves <dannymcwaves96@gmail.com>
2017-12-25 13:26:06 +0000

    doubly linked list

---
+++ structs/linked_list/_dindex.py
@@ -0,0 +1,9 @@
+
+__all__ = []
+from ._index import LinkedList
+
+
+class DoubleLinkedList(LinkedList):
+    """
+    this inherits all from  the Linked list defined earlier, just inserts, appends, and deletes.
+    """
