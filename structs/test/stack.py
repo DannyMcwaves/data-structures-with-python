49c1825dc23edaebf8d8bfaa3d2afd667d252653
dannymcwaves <dannymcwaves96@gmail.com>
2018-01-01 20:15:17 +0000

    tests for stack

---
+++ structs/test/stack.py
@@ -0,0 +1,12 @@
+
+from structs import Stack
+
+stack = Stack()
+stack.push(12)
+stack.push(13)
+
+print(stack.pop())
+print(stack.peek())
+print(stack.top)
+print(stack.peek())
+print(stack.isEmpty())
