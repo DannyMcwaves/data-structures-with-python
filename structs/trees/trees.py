2abfe26cb3553da95dae87a09fecfaf4c518f686
dannymcwaves <dannymcwaves96@gmail.com>
2018-01-25 12:43:39 +0000

    the Tree class

---
+++ structs/trees/trees.py
@@ -0,0 +1,16 @@
+__all__ = ["Tree"]
+
+
+class Tree:
+
+    root = None
+    left = None
+    right = None
+    val = None
+
+    def __init__(self, root):
+        """
+        this is the realest.
+        """
+        self.val = root
+
